fb081aa899910fbabb1dc117cc6b03871ecf5147
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-11-12 20:22:11 +0530

    first commit

---
+++ ProjDemo/src/main/java/com/Flight.java
@@ -0,0 +1,44 @@
+package com;
+
+import java.awt.print.Printable;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Flight
+ */
+public class Flight extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Flight() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String date= request.getParameter("udate");
+		PrintWriter out=response.getWriter();
+		out.print("Date :"+date );
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
